67427f611172c5abdcdaa2abf05a14b2454de459
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 00:53:36 +0530

    Refactor displayName getter in FileCategoryExtension for improved readability

--- lib/models/file_category.dart
+++ lib/models/file_category.dart
@@ -1,6 +1,18 @@
 enum FileCategory { video, image, music, document, other }
+
 extension FileCategoryExtension on FileCategory {
   String get displayName {
-    
+    switch (this) {
+      case FileCategory.video:
+        return 'Video';
+      case FileCategory.image:
+        return 'Image';
+      case FileCategory.music:
+        return 'Music';
+      case FileCategory.document:
+        return 'Document';
+      case FileCategory.other:
+        return 'Other';
+    }
   }
 }
