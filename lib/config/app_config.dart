68a1788dd21d93945d95336b05c482f7aa182719
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 00:51:43 +0530

    Add AppConfig class for environment variable management

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,3 +1,12 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class AppConfig {}
+class AppConfig {
+  static String get azureStorageAccount =>
+      dotenv.env['AZURE_STORAGE_ACCOUNT'] ?? '';
+  static String get azureStorageContainer =>
+      dotenv.env['AZURE_STORAGE_CONTAINER'] ?? '';
+  static String get azureSaSToken => dotenv.env['AZURE_SAS_TOKEN'] ?? '';
+  static String get appName => dotenv.env['APP_NAME'] ?? 'Azure Blob Manager';
+  static String get privateSectionPassword =>
+      dotenv.env['PRIVATE_SECTION_PASSWORD'] ?? '';
+}
