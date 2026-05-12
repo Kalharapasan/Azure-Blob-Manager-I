a8a794c9da091259262d6c0f1f1c2bac0a28205c
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 01:02:18 +0530

    Add import for file_provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'config/app_config.dart';
+import 'providers/file_provider.dart';
 import 'screens/dashboard_screen.dart';
