1fe45bb17e2f4f2cb8ae6d3e2873f69c4779ad0d
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 01:09:32 +0530

    Update FileUploadDialog to use AboutDialog instead of Placeholder

--- lib/widgets/file_upload_dialog.dart
+++ lib/widgets/file_upload_dialog.dart
@@ -21,8 +21,12 @@ class _FileUploadDialogState extends State<FileUploadDialog> {
 
   final List<String> _categories = ['video', 'image', 'music', 'document', 'other'];
 
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AboutDialog(
+      
+    );
   }
 }
