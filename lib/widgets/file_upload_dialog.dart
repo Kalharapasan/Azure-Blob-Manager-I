08c05f25b7539d74833e4b8baf456f07d14aaa5c
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 01:07:41 +0530

    Add FileUploadDialog widget with basic structure and placeholder

--- lib/widgets/file_upload_dialog.dart
+++ lib/widgets/file_upload_dialog.dart
@@ -4,3 +4,16 @@ import 'package:file_picker/file_picker.dart';
 import 'package:provider/provider.dart';
 import '../providers/file_provider.dart';
 
+class FileUploadDialog extends StatefulWidget {
+  const FileUploadDialog({super.key});
+
+  @override
+  State<FileUploadDialog> createState() => _FileUploadDialogState();
+}
+
+class _FileUploadDialogState extends State<FileUploadDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
