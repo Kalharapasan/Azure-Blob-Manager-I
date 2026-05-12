d34a85cb72d3132bb01456721292f3797a2b535e
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 01:06:37 +0530

    Add file_upload_dialog.dart with necessary imports for file upload functionality

---
+++ lib/widgets/file_upload_dialog.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:provider/provider.dart';
+import '../providers/file_provider.dart';
+
