b5cfb6fff50b941ebe60b33e7e40fecb807166b2
Pasan Kalhara <kalharap84@gmail.com>
2026-05-13 01:02:53 +0530

    Refactor MyApp class structure in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import 'package:provider/provider.dart';
 import 'config/app_config.dart';
 import 'providers/file_provider.dart';
 import 'screens/dashboard_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
