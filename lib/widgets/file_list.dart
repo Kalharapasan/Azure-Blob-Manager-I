3ef7d7cd66d9916fd66cab481dba7bec89022146
Pasan Kalhara <kalharap84@gmail.com>
2026-05-14 22:51:58 +0530

    Refactor FileList widget to use ListView.builder for improved performance and simplify file item display

--- lib/widgets/file_list.dart
+++ lib/widgets/file_list.dart
@@ -13,42 +13,12 @@ class FileList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8),
-      child: ListTile(
-        leading: _getFileIcon(file.category),
-        title: Text(file.name),
-        subtitle: Text(
-          'Size: ${(file.size / 1024).toStringAsFixed(2)} KB\n'
-          'Uploaded: ${file.uploadedAt.toLocal()}',
-        ),
-        trailing: PopupMenuButton<String>(
-          onSelected: (value) async {
-            if (value == 'delete') {
-              await _deleteFile(context, file);
-            } else if (value == 'download') {
-              await _downloadFile(context, file);
-            }
-          },
-          itemBuilder: (context) => [
-            const PopupMenuItem(
-              value: 'download',
-              child: ListTile(
-                leading: Icon(Icons.download),
-                title: Text('Download'),
-              ),
-            ),
-            const PopupMenuItem(
-              value: 'delete',
-              child: ListTile(
-                leading: Icon(Icons.delete),
-                title: Text('Delete'),
-              ),
-            ),
-          ],
-        ),
-        onTap: () => _openFile(context, file),
-      ),
+    return ListView.builder(
+      itemCount: displayedFiles.length,
+      itemBuilder: (context, index) {
+        final file = displayedFiles[index];
+        return FileItemWidget(file: file);
+      },
     );
   }
 }
