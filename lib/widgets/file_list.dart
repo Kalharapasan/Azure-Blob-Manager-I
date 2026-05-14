02f288d3caf5d4544e1f77c5fd88570df9cc2845
Pasan Kalhara <kalharap84@gmail.com>
2026-05-14 22:40:28 +0530

    Add FileList widget to display file items

--- lib/widgets/file_list.dart
+++ lib/widgets/file_list.dart
@@ -2,3 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/file_provider.dart';
 import '../models/file_item.dart';
+
+class FileList extends StatelessWidget {
+  
+  const FileList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
