440506db9ce6566ff269a3b36faf34cebf992221
Pasan Kalhara <kalharap84@gmail.com>
2026-05-14 23:24:55 +0530

    Add private fields for account name, SAS token, container name, and base URL in AzureBlobService

--- lib/services/azure_blob_service.dart
+++ lib/services/azure_blob_service.dart
@@ -4,6 +4,11 @@ import '../models/file_item.dart';
 import '../config/app_config.dart';
 
 class AzureBlobService {
+  late final String _accountName;
+  late final String _sasToken;
+  late final String _containerName;
+  late final String _baseUrl;
+
   
 
 }
