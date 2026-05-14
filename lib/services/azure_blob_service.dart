81466a7345afefa534b1dbd290e1ad71d7a52fa8
Pasan Kalhara <kalharap84@gmail.com>
2026-05-14 23:26:58 +0530

    Read file bytes in uploadFile method before uploading

--- lib/services/azure_blob_service.dart
+++ lib/services/azure_blob_service.dart
@@ -24,6 +24,7 @@ class AzureBlobService {
           : '$category/$fileName';
 
       final Uri url = Uri.parse('$_baseUrl/$blobPath');
+      final List<int> fileBytes = await file.readAsBytes();
     } catch (e) {
       
     }
