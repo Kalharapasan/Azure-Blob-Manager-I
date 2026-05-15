557d591d33c207a6584c2465c7c665536cb828de
Pasan Kalhara <kalharap84@gmail.com>
2026-05-15 23:40:28 +0530

    Add selectedCategory and onCategorySelected properties to CategorySidebar widget

--- lib/widgets/category_sidebar.dart
+++ lib/widgets/category_sidebar.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CategorySidebar extends StatelessWidget {
+
+  final String selectedCategory;
+  final ValueChanged<String> onCategorySelected;
+
   const CategorySidebar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  IconData _getIconForCategory(String category) {
+    
   }
 }
