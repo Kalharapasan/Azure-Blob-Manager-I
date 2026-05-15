0250a1405020917ddb9dcb242dcaa7fe08b8c0c4
Pasan Kalhara <kalharap84@gmail.com>
2026-05-15 23:39:41 +0530

    Add CategorySidebar widget with basic structure and Placeholder

--- lib/widgets/category_sidebar.dart
+++ lib/widgets/category_sidebar.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class CategorySidebar extends StatelessWidget {
+  const CategorySidebar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
