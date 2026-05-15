62f3732e95d0ca80567e067fb9468da4d7aba699
Pasan Kalhara <kalharap84@gmail.com>
2026-05-15 23:46:23 +0530

    Implement StorageChart widget with initial build method

--- lib/widgets/storage_chart.dart
+++ lib/widgets/storage_chart.dart
@@ -3,3 +3,14 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:provider/provider.dart';
 import '../providers/file_provider.dart';
 
+class StorageChart extends StatelessWidget {
+  const StorageChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final fileProvider = Provider.of<FileProvider>(context);
+    
+    return const Placeholder();
+  }
+}
