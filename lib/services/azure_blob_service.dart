ad92e475f90c09c77840ba44f830a9f20b213025
Pasan Kalhara <kalharap84@gmail.com>
2026-05-15 23:14:45 +0530

    Add validation for end index in blob name extraction loop in listFiles method of AzureBlobService class

--- lib/services/azure_blob_service.dart
+++ lib/services/azure_blob_service.dart
@@ -104,6 +104,9 @@ class AzureBlobService {
         
         for (final String line in blobLines) {
           final int endIndex = line.indexOf('</Name>');
+          if (endIndex > 0) {
+            
+          }
         }
 
       }else{
