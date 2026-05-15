b3b4f8f81ebe1ad351b57f0a9203c6bea9abde42
Pasan Kalhara <kalharap84@gmail.com>
2026-05-15 23:41:00 +0530

    Remove unnecessary whitespace and format _getIconForCategory method for clarity

--- lib/widgets/category_sidebar.dart
+++ lib/widgets/category_sidebar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CategorySidebar extends StatelessWidget {
-
   final String selectedCategory;
   final ValueChanged<String> onCategorySelected;
 
@@ -14,7 +13,20 @@ class CategorySidebar extends StatelessWidget {
 
   IconData _getIconForCategory(String category) {
     switch (category) {
-      
+      case 'all':
+        return Icons.storage;
+      case 'video':
+        return Icons.videocam;
+      case 'image':
+        return Icons.image;
+      case 'music':
+        return Icons.music_note;
+      case 'document':
+        return Icons.description;
+      case 'other':
+        return Icons.folder;
+      default:
+        return Icons.file_present;
     }
   }
 }
