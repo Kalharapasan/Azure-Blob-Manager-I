ee6b894373c8f3d90be114fa3e7d16506ba9872a
Pasan Kalhara <kalharap84@gmail.com>
2026-05-16 14:07:51 +0530

    feat: initialize Android application build configuration and manifest permissions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,25 +6,22 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app"
+    namespace = "com.azureblobmanager.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        applicationId = "com.azureblobmanager.app"
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
