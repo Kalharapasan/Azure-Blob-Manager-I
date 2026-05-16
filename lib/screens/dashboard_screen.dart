40470bf55d2ceb94d60fec25520531a845e001e7
Pasan Kalhara <kalharap84@gmail.com>
2026-05-16 11:54:15 +0530

    Add initial import statements to dashboard_screen.dart

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/file_provider.dart';
+import '../widgets/category_sidebar.dart';
+import '../widgets/file_list.dart';
+import '../widgets/file_upload_dialog.dart';
+import '../widgets/storage_chart.dart';
+import '../widgets/private_section.dart';
+import '../config/app_config.dart';
